18101ff24e8256011152019a949f3523fcb82381
Dan Goff <dgoff@urbn.com>
2024-02-28 13:02:16 -0500

    Adding reference to provider-version module

--- main.tf
+++ main.tf
@@ -15,7 +15,8 @@
  */
 
 data "google_compute_global_address" "default" {
-  name = element(concat(google_compute_global_address.default.*.name, list(var.ip_address_name)), 0)
+  provider = module.provider-version.google
+  name     = element(concat(google_compute_global_address.default.*.name, list(var.ip_address_name)), 0)
 }
 
 resource "google_compute_global_address" "default" {
