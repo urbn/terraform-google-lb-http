c654792ae180c2847ec2e558dd6491ee7cc77752
Steve Miskiewicz <smiskiewicz@urbn.com>
2025-03-24 08:50:26 -0400

    add multi sec polices

--- variables.tf
+++ variables.tf
@@ -88,3 +88,9 @@ variable "https_redirect" {
   type        = bool
   default     = false
 }
+
+variable "backend_security_policies" {
+  description = "List of security policies for each backend. If not provided, the default security policy will be used."
+  type        = list(string)
+  default     = []
+}
